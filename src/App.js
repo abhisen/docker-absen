2f3d02c0a70d5a738352326f02753b6667b78f12
Abhijit Sen <abhijitsen@Abhijits-MacBook-Air.local>
2020-12-17 18:14:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Abhijit Sen ... Test Docker!
+	  App file updated at runtime!!! 1..2..3..4..5
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
